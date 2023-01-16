37ae1379b305cc70f2ae0f6bdf1d9b5db784e159
Raju Sah <rajukumar.sjce@gmail.com>
2023-01-16 22:01:54 +0530

    first commit

---
+++ src/test/java/com/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("testcase executed");
+		assertEquals(true, true);
+		
+	}
+
+}
